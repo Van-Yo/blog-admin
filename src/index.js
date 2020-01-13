27ffa99add692fd3b7063aeeaac65765026d232f
Van-Yo <jokerwanr@gmail.com>
2020-01-13 22:01:38 +0800

    利用antdUI组件完成登录和首页的初步布局

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Main from './pages/Main';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Main />, document.getElementById('root'));
